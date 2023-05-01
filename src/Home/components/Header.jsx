64e8cebf58f3065c3f5106a8812c8ddc6c40df36
willrizzo <willianrizzoratao10@gmail.com>
2023-05-01 02:11:02 -0300

    Header hovers

--- src/Home/components/Header.jsx
+++ src/Home/components/Header.jsx
@@ -7,20 +7,20 @@ function Header(){
             <div className=" w-full bg-gray-200 flex">
                 <img className='p-4 ml-10' src={Logo} alt="" />
                 <div className='flex gap-10 items-center'>
-                    <p className='text-lg text-[#FFAD0D]'>Compre Cripto</p>
-                    <p className='text-lg text-[#FFAD0D]'>Mercado</p>
-                    <p className='text-lg text-[#FFAD0D]'>NFT</p>
+                    <p className='text-lg text-[#FFAD0D] hover:text-black transition-all hover:scale-110 duration-200'>Compre Cripto</p>
+                    <p className='text-lg text-[#FFAD0D] hover:text-black transition-all hover:scale-110 duration-200'>Mercado</p>
+                    <p className='text-lg text-[#FFAD0D] hover:text-black transition-all hover:scale-110 duration-200'>NFT</p>
                 </div>
 
                 <div className='flex gap-10 items-center ml-auto mr-10'>
-                    <button className='text-lg text-[#FFAD0D]'>Entrar</button>
-                    <button className='text-lg text-[#FFAD0D] bg-[#111111] bg-opacity-80 p-2 rounded-md' >Abra sua conta</button>
+                    <button className='text-lg text-[#FFAD0D] hover:text-black transition-all hover:scale-110 duration-200'>Entrar</button>
+                    <button className='text-lg text-[#FFAD0D] bg-[#111111] bg-opacity-80 p-2 rounded-md hover:bg-[#FFAD0D] hover:text-white transition-all hover:scale-110 duration-200' >Abra sua conta</button>
                     <img src={Modo} alt="" />
                 </div>
             </div>
             
             <div className='w-full bg-[#111111] bg-opacity-80'>
-                <p className='text-center text-[#FFAD0D] opacity-80 text-sm p-1.5' >Registre-se agora — Ganhe até 100 USDT de desconto na taxa de trading (para usuários verificados) - <span className='border-b-2 border-[#FFAD0D]'>Saiba Mais!</span></p>
+                <p className='text-center text-[#FFAD0D] opacity-80 text-sm p-1.5' >Registre-se agora — Ganhe até 100 USDT de desconto na taxa de trading (para usuários verificados) - <span className='border-b-2 border-[#FFAD0D] hover:text-white transition-all duration-200'>Saiba Mais!</span></p>
                 
             </div>
         </div>

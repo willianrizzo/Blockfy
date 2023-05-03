a768e120078a22106b7254eaed878c536c093f88
willrizzo <willianrizzoratao10@gmail.com>
2023-05-03 11:48:05 -0300

    teste

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import Cards from "./Home/components/Cards"
 import Footer from "./Home/components/Footer"
 import Section from "./Home/components/Section"
 import Suporte from "./Home/components/Suporte"
-import Header from "./Home/components/header"
+import Header from "./Home/components/Header"
 
 function Home(){
     return(

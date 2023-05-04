b730e306beace7bf65377b38686498734df32749
willrizzo <willianrizzoratao10@gmail.com>
2023-05-04 18:35:08 -0300

    icones e funções dashboard

--- src/Dashboard/components/LateralEsq.jsx
+++ src/Dashboard/components/LateralEsq.jsx
@@ -1,13 +1,34 @@
 import Logo from '../../assets/logo.png'
 import Casa from '../../assets/casa.png'
+import Grafico from '../../assets/grafico.png'
+import Carteira from '../../assets/carteira.png'
+import Perfil from '../../assets/perfil.png'
 
 function LateralEsq(){
     return(
-        <div className='w-[20%] h-[100vh] bg-slate-300'>
+        <div className='w-[20%] h-[100vh] bg-gray-100'>
             <img className='w-[700px] pt-10 pl-14' src={Logo} alt="" />
 
-            <div className='w-[75%] bg-slate-500 m-auto mt-[45%]'>
-                
+            <div className='w-[80%] mt-[45%] flex flex-col gap-14'>
+                <div className='w-[50%] e m-auto flex  gap-5'>
+                    <img src={Casa} alt="" />
+                    <p className='text-lg font-semibold'>Dashboard</p>
+                </div>
+
+                <div className='w-[50%] e m-auto flex  gap-5'>
+                    <img src={Grafico} alt="" />
+                    <p className='text-lg text-gray-700'>Estatísticas</p>
+                </div>
+
+                <div className='w-[50%] e m-auto flex gap-5'>
+                    <img src={Carteira} alt="" />
+                    <p className='text-lg font-semibold'>Carteira</p>
+                </div>
+
+                <div className='w-[50%] e m-auto flex gap-5 items-center'>
+                    <img src={Perfil} alt="" />
+                    <p className='text-lg font-semibold'>Perfil</p>
+                </div>
             </div>
         </div>
         

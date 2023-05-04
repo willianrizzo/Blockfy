84a35848606c9ab25c66d70b5e1b792c2881cbf2
willrizzo <willianrizzoratao10@gmail.com>
2023-05-04 18:21:52 -0300

    Criando Dashboard

---
+++ src/Dashboard/components/LateralEsq.jsx
@@ -0,0 +1,15 @@
+import Logo from '../../assets/logo.png'
+import Casa from '../../assets/casa.png'
+
+function LateralEsq(){
+    return(
+        <div className='w-[20%] h-[100vh] bg-slate-300'>
+            <img className='w-[700px] pt-10 pl-14' src={Logo} alt="" />
+
+            <div className='w-[75%] bg-slate-500 m-auto mt-[45%]'>
+                
+            </div>
+        </div>
+        
+    )
+}export default LateralEsq

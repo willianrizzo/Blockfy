4a3e57e30bf18fff07fcaece8be875eca5854eba
willrizzo <willianrizzoratao10@gmail.com>
2023-05-04 17:22:44 -0300

    finalizando login e cadastro

--- src/Cadastro/components/Section.jsx
+++ src/Cadastro/components/Section.jsx
@@ -14,19 +14,32 @@ function Section(){
             
             <img className='m-auto w-[15%] absolute left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 ' src={Be} alt="" />
             <div className='w-[35%] bg-gray-300 absolute left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 backdrop-blur-sm bg-opacity-40 rounded-2xl  '>
-                <h3 className='text-center font-semibold text-3xl p-5'>Login</h3>
+                <h3 className='text-center font-semibold text-3xl p-5'>Realize seu Cadastro!</h3>
                 <div className='flex flex-col justify-center gap-10 mt-6 items-center'>
-                    <input placeholder='CPF' className='w-[60%] h-[20px] bg-black text-white bg-opacity-70 rounded-full p-5' type="text" maxLength={11} />
-                    <input placeholder='Senha' className='w-[60%] h-[20px] bg-black text-white bg-opacity-70 rounded-full p-5' type="password" />
+                    <input placeholder='Digite seu nome completo' className='w-[70%] h-[20px] bg-black text-white bg-opacity-70 rounded-full p-5' type="text" />
                 </div> 
-                <div className='flex gap-10 h-10 mt-10 justify-center'>
+
+                <div className='flex justify-center gap-10 mt-10 items-center'>
+                    <input placeholder='Digite seu CPF' className='w-[30%] h-[20px] bg-black text-white bg-opacity-70 rounded-full p-5' type="text" maxLength={11} />
+                    <input placeholder='Data de Nascimento' className='w-[30%] h-[20px] bg-black text-white bg-opacity-70 rounded-full p-5' type="date" />
+                </div>
+
+                <div className='flex justify-center gap-10 mt-10 items-center'>
+                    <input placeholder='Digite sua senha' className='w-[30%] h-[20px] bg-black text-white bg-opacity-70 rounded-full p-5' type="password" />
+                    <input placeholder='Redigite a senha' className='w-[30%] h-[20px] bg-black text-white bg-opacity-70 rounded-full p-5' type="password" />
+                </div>
+
+               <div className='mt-6'>
+                    <h3 className='text-center font-semibold'>Ou cadastre-se com:</h3>
+               </div>
+                <div className='flex gap-10 h-10 mt-4 justify-center'>
                     <button className='bg-black bg-opacity-70 text-white w-[140px] flex justify-center items-center gap-2 rounded-full  transition-all duration-300 hover:scale-110 hover:invert'><img src={Google} alt="" />Google</button>
                     <button className='bg-black bg-opacity-70 text-white w-[140px] flex justify-center items-center gap-2 rounded-full   transition-all duration-300 hover:scale-110 hover:invert'><img src={Apple} alt="" />Apple</button>
                 </div>
 
                 <div className=' flex flex-col justify-center items-center w-full mt-12 gap-7 '>
-                    <button className='w-[130px] h-[35px] bg-black text-white bg-opacity-70 rounded-full  transition-all duration-300 hover:scale-110 hover:invert'>Logar</button>
-                    <p className='font-medium pb-5 transition-all duration-300 hover:invert'>Não possui conta? <span className='text-white border-b-2 border-black transition-all duration-300 hover:invert'>Cadrastre-se!</span></p>
+                    <Link to='/login' className='w-[130px] h-[35px] bg-black text-white bg-opacity-70 rounded-full  transition-all duration-300 hover:scale-110 hover:invert flex justify-center items-center'>Cadastrar</Link>
+                    <p className='font-medium pb-5 transition-all duration-300 hover:invert'>Já possui uma conta? <Link to='/login' className='text-white border-b-2 border-black transition-all duration-300 hover:invert'>Faça seu Login!</Link></p>
                 </div>
                 
             </div>
--- src/Login/componentes/Section.jsx
+++ src/Login/componentes/Section.jsx
@@ -16,8 +16,8 @@ function Section(){
             <div className='w-[35%] bg-gray-300 absolute left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 backdrop-blur-sm bg-opacity-40 rounded-2xl  '>
                 <h3 className='text-center font-semibold text-3xl p-5'>Login</h3>
                 <div className='flex flex-col justify-center gap-10 mt-6 items-center'>
-                    <input placeholder='CPF' className='w-[60%] h-[20px] bg-black text-white bg-opacity-70 rounded-full p-5' type="text" maxLength={11} />
-                    <input placeholder='Senha' className='w-[60%] h-[20px] bg-black text-white bg-opacity-70 rounded-full p-5' type="password" />
+                    <input placeholder='Digite seu CPF' className='w-[60%] h-[20px] bg-black text-white bg-opacity-70 rounded-full p-5' type="text" maxLength={11} />
+                    <input placeholder='DIgite sua senha' className='w-[60%] h-[20px] bg-black text-white bg-opacity-70 rounded-full p-5' type="password" />
                 </div> 
                 <div className='flex gap-10 h-10 mt-10 justify-center'>
                     <button className='bg-black bg-opacity-70 text-white w-[140px] flex justify-center items-center gap-2 rounded-full  transition-all duration-300 hover:scale-110 hover:invert'><img src={Google} alt="" />Google</button>
@@ -25,8 +25,8 @@ function Section(){
                 </div>
 
                 <div className=' flex flex-col justify-center items-center w-full mt-12 gap-7 '>
-                    <button className='w-[130px] h-[35px] bg-black text-white bg-opacity-70 rounded-full  transition-all duration-300 hover:scale-110 hover:invert'>Logar</button>
-                    <p className='font-medium pb-5 transition-all duration-300 hover:invert'>Não possui conta? <span className='text-white border-b-2 border-black transition-all duration-300 hover:invert'>Cadrastre-se!</span></p>
+                    <Link to='/' className='w-[130px] h-[35px] bg-black text-white bg-opacity-70 rounded-full  transition-all duration-300 hover:scale-110 hover:invert flex justify-center items-center'>Logar</Link>
+                    <p className='font-medium pb-5 transition-all duration-300 hover:invert'>Não possui conta? <Link to='/cadastro' className='text-white border-b-2 border-black transition-all duration-300 hover:invert'>Cadrastre-se!</Link></p>
                 </div>
                 
             </div>
